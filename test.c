5952c906698a08a0121c506ef815c2292ecfd9eb
Aircraft <169865680@qq.com>
2019-11-27 14:16:43 +0800

    test

---
+++ test.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+void print(){
+   char arr[10];
+   memcpy(arr,"sas",1);
+  
+}
+int main(){
+
+  printf("%d",20);
+  if(1){
+    printf("youxi");
+  }
+  print();
+  printf("hello world!!!");
+  printf("hello world!!!");
+
+  return 0;  
+}
