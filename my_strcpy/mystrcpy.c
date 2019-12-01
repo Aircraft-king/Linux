373a2f8eece60209c65341a0007e876546aedbbd
Aircraft <169865680@qq.com>
2019-12-01 10:56:24 +0800

    mystrcpy

---
+++ my_strcpy/mystrcpy.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<assert.h>
+//3.模拟实现strcpy
+char* my_strcpy(char* a1, const char* a2) {
+  char* ret = a1;
+  assert(a1 != NULL);
+  assert(a2 != NULL);
+  while (*a1++ = *a2++) {
+    ;
+  }
+  return ret;
+}
+
+int main() {
+  char arr1[11] = "\0";
+  char arr2[11] = "abcdefghij";
+  char* a = my_strcpy(arr1, arr2);
+  printf("%s", a);
+  return 0;
+}
